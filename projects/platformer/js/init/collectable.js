0b8b8c792703049e91e19fcf3cbe0fa1fc22bf4c
JohnnyWhite12 <jlw0202321@lpssonline.com>
2022-10-06 18:37:01 +0000

    added platforms and collectables to game

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -37,10 +37,14 @@
 
     ////////////////////////////////////////////////////////////////////////
     // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-
+    createCollectable(type, x, y, gravity, bounce)
     // example:
     createCollectable(type.steve, 200, 170, 6, 0.7);
-
+    type.db         // worth 10 points
+    type.max        // worth 20 points
+    type.steve      // worth 30 points
+    type.grace      // worth 40 points
+    type.kennedi    // worth 50 points
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
